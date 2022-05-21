9e613d8d2a9939ab2ebbb57e5e903e6e95084df9
徐斯远 <xusiyuan@shoplazza.com>
2022-05-22 00:02:43 +0800

    fix: eslint & call chrome API

--- src/background/main.js
+++ src/background/main.js
@@ -2,3 +2,18 @@ import hotReload from '@/utils/hotReload'
 hotReload()
 
 console.log('hello world background')
+
+// 激活页面时yidong移动到第一个tab
+chrome.tabs.onActivated.addListener(activeInfo => move(activeInfo));
+
+async function move(activeInfo) {
+  try {
+    await chrome.tabs.move(activeInfo.tabId, {index: 0});
+    console.log('Success.');
+  } catch (error) {
+    if (error == 'Error: Tabs cannot be edited right now (user may be dragging a tab).') {
+      setTimeout(() => move(activeInfo), 50);
+    }
+  }
+}
+
